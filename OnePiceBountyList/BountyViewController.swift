88e536b7651f244e42a254af625c7e80789d03b6
59 <5.9.devv@gmail.com>
2022-02-18 22:48:40 +0900

    다음 페이지로 데이터 보내기 및 다음페이지 화면에 표시하기

--- OnePiceBountyList/BountyViewController.swift
+++ OnePiceBountyList/BountyViewController.swift
@@ -12,6 +12,26 @@ class BountyViewController: UIViewController {
     let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
     let bountyList = [3300000, 50, 4400000, 300000000, 1600000, 8000000, 7700000, 120000000]
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 이 함수는 아래의 Delegate가 실행되고 나서, Modal을 띄우기 전에 준비하는 함수이다.
+        // 따라서, Delegate에서 이 쪽으로 'sender'를 통해 데이터를 보내줄 수 있다.
+        // 필요한 데이터는 'indexPath.row' 이므로, Delegate의 performSegue에서 sender로 설정해준다.
+        
+        // DetailViewController에게 데이터 전달해주기
+        // 먼저 어떤 Segue에게 전달할지 설정해준다.
+        if segue.identifier == "showDetailModally" {
+            // segue의 목적지가 DetailViewController라는 것을 변수(vc)로 선언해주고,
+            let vc = segue.destination as? DetailViewController
+            // Delegate 에서 전달받은 sender를 Int로 다운캐스팅 한다.
+            if let index = sender as? Int {
+                // 미리 DetailViewController에 선언해놓은 name, bounty 변수에 각각 값을 할당해준다.
+                vc?.name = nameList[index]
+                vc?.bounty = bountyList[index]
+            }
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -45,10 +65,12 @@ extension BountyViewController: UITableViewDataSource {
 extension BountyViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("------> 현재 번호: \(indexPath.row)")
+        
         // withIdentifier에 설정한 Segue Identifier 이름을 넣어준다.
-        // sender는 Segue를 수행하는데 있어서 특정 Object를 함께 보낼 수 있는데,
-        // 현재는 nil로 설정하면 된다.
-        performSegue(withIdentifier: "showDetailModally", sender: nil)
+        // sender는 Segue를 수행하는데 있어서 특정 Object를 함께 보낼 수 있다.
+        // Modal에 정보를 보내줘야 하므로, sender에 indexPath.row를 넣어 보내준다.
+        // 여기서 보낸 정보는, 상단 'prepare' 함수로 전달된다.
+        performSegue(withIdentifier: "showDetailModally", sender: indexPath.row)
     }
 }
 
--- OnePiceBountyList/DetailViewController.swift
+++ OnePiceBountyList/DetailViewController.swift
@@ -9,14 +9,30 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+    
+    var name: String?
+    var bounty: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+    
+    func updateUI() {
+        if let name = self.name, let bounty = self.bounty {
+            let image = UIImage(named: "\(name).jpg")
+            imageView.image = image
+            nameLabel.text = name
+            bountyLabel.text = "\(bounty)"
+        }
     }
     
     @IBAction func closeBtn(_ sender: UIButton) {
         // dismiss는 사라지게 하는 것을 의미한다.
         // 사라진 후, 동작하게 될 것들을 정의해줄 수 있다.
         dismiss(animated: true, completion: nil)
-        
     }
 }
