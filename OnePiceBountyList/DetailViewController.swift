53ca502663ef6107dbd245a9fe20c4f9a34f4ab1
59 <5.9.devv@gmail.com>
2022-02-27 00:55:15 +0900

    애니메이션 적용하기

--- OnePiceBountyList/DetailViewController.swift
+++ OnePiceBountyList/DetailViewController.swift
@@ -23,6 +23,10 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
     
+    // 애니메이션을 적용하기 위해 Constraint를 가져온다.
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
+    
 //    var name: String?
 //    var bounty: Int?
     
@@ -33,7 +37,91 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        // 애니메이션 준비
+        prepareAnimation2()
     }
+    
+    // 뷰가 뜬 다음에 실행할 것
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // 애니메이션 실행
+        showAnimation2()
+    }
+    
+    // 뷰 속성을 이용한 애니메이션 구현
+    private func prepareAnimation2() {
+        // 우측 화면밖에서 가운데로 이동하고, 크기도 컸다가 작아진다. 그리고, 회전하면서 들어온다.
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0)
+            .scaledBy(x: 3, y: 3).rotated(by: 180)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0)
+            .scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        // 투명하게 만들어준다.
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
+    }
+    
+    // 뷰 속성을 이용한 애니메이션 구현
+    private func showAnimation2() {
+        
+        // nameLabel 애니메이션 주기
+        UIView.animate(withDuration: 0.8, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            // Label에 변형을 가하기 전, 원래 형태
+            self.nameLabel.transform = CGAffineTransform.identity
+            // 투명하게 만들었던 것을, 원래대로 돌려놓는다.
+            self.nameLabel.alpha = 1
+        }, completion: nil)
+        
+        // nameLabel 이후에, bounty Label 애니메이션 주기
+        UIView.animate(withDuration: 0.8, delay: 0.4, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            // Label에 변형을 가하기 전, 원래 형태
+            self.bountyLabel.transform = CGAffineTransform.identity
+            // 투명하게 만들었던 것을, 원래대로 돌려놓는다.
+            self.bountyLabel.alpha = 1
+        }, completion: nil)
+        
+        // 이미지 회전시키기
+        UIView.transition(with: imageView, duration: 0.3, options: .transitionFlipFromLeft, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        
+    }
+    
+    
+    // 제약을 이용한 애니메이션 구현
+    private func prepareAnimation() {
+        // 화면 밖으로 nameLabel을 내보내준다.
+        nameLabelCenterX.constant = view.bounds.width
+        bountyLabelCenterX.constant = view.bounds.width
+    }
+    
+    // 제약을 이용한 애니메이션 구현
+    private func showAnimation() {
+        nameLabelCenterX.constant = 0
+        bountyLabelCenterX.constant = 0
+        
+        // 기본 애니메이션
+//        UIView.animate(withDuration: 0.3) {
+//            self.view.layoutIfNeeded()
+//        }
+////
+        // 애니메이션 옵션 추가
+//        UIView.animate(withDuration: 0.3, delay: 0.1, options: .curveEaseIn, animations: {
+//            self.view.layoutIfNeeded()
+//        }, completion: nil)
+        
+        // 스프링 반동효과
+        UIView.animate(withDuration: 0.5, delay: 0.3, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {self.view.layoutIfNeeded()}, completion: nil)
+        
+        // 이미지 회전시키기
+        UIView.transition(with: imageView, duration: 0.3, options: .transitionFlipFromLeft, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        
+    }
+    
     
     func updateUI() {
         if let bountyInfo = viewModel.bountyInfo {
